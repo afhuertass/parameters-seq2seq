6d11e5d6241dd9272effed49363d1bb7d31b9905
Andres Huertas S <afhuertass@unal.edu.co>
2017-08-03 17:22:08 -0500

    Dummy repository to pass parameters to the server, in order to run a seq2seq model on Instacart Data

---
+++ vars.v
@@ -0,0 +1,9 @@
+export VOCAB_SOURCE="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/train/vocab.sources.txt"
+export VOCAB_TARGET="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/train/vocab.targets.txt"
+export TRAIN_SOURCES="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/train/sources.txt"
+export TRAIN_TARGETS="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/train/targets.txt"
+export DEV_SOURCES="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/dev/sources.txt"
+export DEV_TARGETS="/home/andresh/data-science/dnc-seq2seq/nmt_data/toy_reverse/dev/targets.txt"
+export MODEL_DIR="/home/andresh/data-science/dnc-seq2seq/model_output"
+		  
+		     
