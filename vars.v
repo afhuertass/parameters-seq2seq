c1a514cb7474d95dc1ce5c4b760304219887d162
Andres Huertas S <afhuertass@unal.edu.co>
2017-08-04 11:22:14 -0500

    #

--- vars.v
+++ vars.v
@@ -7,5 +7,7 @@ export TRAIN_TARGETS="/mnt/data/part12/afhuertass/home/instacart-data/train_targ
 export DEV_SOURCES="/mnt/data/part12/afhuertass/home/instacart-data/dev_sources.txt"
 export DEV_TARGETS="/mnt/data/part12/afhuertass/home/instacart-data/dev_targets.txt"
 
+export TEST_SOURCES="/mnt/data/part12/afhuertass/home/instacart-data/test_sources.txt"
 export MODEL_DIR="/mnt/data/part12/afhuertass/home/instacart-seq2seq/model_output"
 		     
+export LD_LIBRARY_PATH=/mnt/data/part12/afhuertass/usr/local/cuda-8.0/extras/CUPTI/lib64:$LD_LIBRARY_PATH
